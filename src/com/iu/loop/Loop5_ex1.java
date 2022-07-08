520d10da73fd7e546d11ba048ada85232a1dc1ae
kimdaeki <kimdaeki@gmail.com>
2022-07-08 17:42:40 +0900

    loop

--- src/com/iu/loop/Loop5_ex1.java
+++ src/com/iu/loop/Loop5_ex1.java
@@ -14,6 +14,7 @@ public class Loop5_ex1 {
 		//로그인 판단 로그인이 성공하면 종료
 		//실패하면 다시 입력
 		boolean check=true;
+		boolean flag = false;
 		while(check) {
 			System.out.println("1. 로그인");
 			System.out.println("2. 종 료");
@@ -28,7 +29,8 @@ public class Loop5_ex1 {
 				if(id==yId&&pw==yPw) {
 					System.out.println("로그인 성공");
 					check=!check;
-					break;
+					flag=!flag;
+					
 				}else {
 					System.out.println("로그인 실패");
 				}
@@ -57,6 +59,36 @@ public class Loop5_ex1 {
 		//5랩 달성시 축하금으로 1000Gold 지급
 		//10랩 달성시 축하금으로 2000Gold 지급
 		//15랩 달성시 축하금으로 3000Gold 지급
+		flag=true;
+		if(flag) { //flag true면 로그인성공, false면 로그인 실패
+			System.out.println("게임을 시작");
+			int level=1;
+			int gold=1000;
+			
+			for(level=1;level<15;level++) {
+				
+				if(level%5==0) {
+					gold = gold+level/5*1000;
+				}
+				
+				
+				for(int mon=0;mon<3*level;mon++) {
+					System.out.println(mon+1+"꽥!!!");
+				}
+				System.out.println("==============");
+				
+			}
+			
+			
+			gold = gold+3000;
+			
+			System.out.println("최종레벨 : "+level);
+			System.out.println("최종Gold : "+gold);
+		}
+		
+		System.out.println("프로그램을 종료 합니다");
+		
+		
 		
 		
 
